bea361082524df437e4465f280746aa4e10b9dee
Rukeh <lucasfourny2@gmail.com>
2026-03-01 20:21:18 +0100

    test config and constants

--- disktest.cpp
+++ disktest.cpp
@@ -1,6 +1,20 @@
+#include <cstddef>
 #include <iostream>
 
-int main() {
-    std::cout << "DiskLens - Disk I/O Tester\n";
+using namespace std;
+
+constexpr size_t KB = 1024;
+constexpr size_t MB = 1024 * KB;
+
+struct TestConfig {
+    string test_dir = "./disklens_test";
+    size_t block_size = 4 * KB;
+    size_t total_size = 64 * MB;
+    size_t rand_ops= 1024;  
+    bool verbose = true;
+    bool cleanup = true;
+};
+
+int main(){
     return 0;
 }
